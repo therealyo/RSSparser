a9df62a3ed7df5affe317f15651c94a401b0cca8
Daniil Pshenik <dpshenik2002@gmail.com>
2023-03-17 11:47:15 +0200

    add: changed cron task interval from every minute to every 3 hours

--- src/cron/processes/RssParser.ts
+++ src/cron/processes/RssParser.ts
@@ -5,7 +5,7 @@ import { CronTask } from '@/interfaces/CronTask';
 import { Post } from '@/database/PostsTable';
 
 class RssParser implements CronTask {
-  public readonly interval = '* * * * *'; // Each day at 9 a.m. Kyiv time
+  public readonly interval = '0 */3 * * *'; // Each day every 3 hours Kyiv time
   private readonly rssParser: Parser<{}, Post> = new Parser<{}, Post>();
 
   public constructor(private posts: Posts) {}
@@ -17,7 +17,6 @@ class RssParser implements CronTask {
       const existing = await this.posts.getExistingIds()
 
       const newPosts = feed.items.filter(item => !existing.includes(item.guid!))
-      console.log(newPosts)
       await this.posts.createPosts(newPosts);
 
     } catch (e: unknown) {
