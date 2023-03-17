29884a46fbbcd1058f7ab2ee793807481fbac8e8
Daniil Pshenik <dpshenik2002@gmail.com>
2023-03-17 10:49:29 +0200

    add: changed posts table structure

--- migrations/0000_green_wiccan.sql
+++ migrations/0000_green_wiccan.sql
@@ -1,11 +1,12 @@
 CREATE TABLE IF NOT EXISTS "posts" (
-	"guid" integer PRIMARY KEY NOT NULL,
+	"guid" varchar PRIMARY KEY NOT NULL,
 	"creator" varchar(100),
 	"link" varchar(256),
 	"pub_date" timestamp(6) with time zone,
 	"content" text,
 	"content_snippet" text,
-	"categories" jsonb
+	"categories" jsonb,
+	"deleted" boolean DEFAULT false
 );
 
 CREATE INDEX IF NOT EXISTS creator_idx ON posts ("creator");
--- src/database/PostsTable.ts
+++ src/database/PostsTable.ts
@@ -1,19 +1,20 @@
 import { index } from "drizzle-orm/pg-core/indexes";
 import { InferModel, pgTable } from "drizzle-orm/pg-core/table";
-import { integer, jsonb, text, timestamp, varchar } from "drizzle-orm/pg-core/columns";
+import { boolean, integer, jsonb, text, timestamp, varchar } from "drizzle-orm/pg-core/columns";
 import { sql } from "drizzle-orm";
 
 export type Post = InferModel<typeof posts, "select">
 export type NewPost = InferModel<typeof posts, "insert">
 
 const posts = pgTable('posts', {
-        guid: integer('guid').primaryKey(),
+        guid: varchar('guid').primaryKey(),
         creator: varchar("creator", {length: 100}),
         link: varchar("link", {length:256}),
         pubDate: timestamp("pub_date", { mode: "string", precision: 6, withTimezone: true }),
         content: text("content"),
         contentSnippet: text("content_snippet"),
         categories: jsonb<string[]>("categories"),
+        deleted: boolean("deleted").default(false)
     }, (posts) => {
         return {
             createIdx: index("creator_idx").on(posts.creator).using(sql``),
